c64787202ee3a76d4ec78ec840970384de496345
danchern97 <danchern97@mail.ru>
2020-09-12 22:57:38 +0300

    Adding first bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python3
+
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
